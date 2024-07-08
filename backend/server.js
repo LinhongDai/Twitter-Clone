fab6f0d0cd261d71a98740fbcc687ec90a975fb4
beebee1 <dbaadaiian@gmail.com>
2024-07-08 15:00:33 -0700

    1- update route files and paths

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,9 @@ import cookieParser from "cookie-parser";
 import {v2 as cloudinary} from "cloudinary";
 
 // routes
-import authRoutes from "./routes/auth.routes.js";
-import userRoutes from "./routes/user.routes.js";
-import postRoutes from "./routes/post.routes.js";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 
